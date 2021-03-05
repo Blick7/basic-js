05f8bb2e89d9665f4351a293fac962368edb9d93
Blick7 <dima.8m8@gmail.com>
2021-03-05 17:39:39 +0200

    feat: dream-team task complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let result = '';
+  if (!Array.isArray(arr))
+    return false;
+  arr.forEach(item => {
+    if (typeof item !== 'string')
+      return false;
+    result += item.trim()[0];
+  });
+  return result.toUpperCase().split('').sort().join('');
 };
+
+console.log(module.exports(['   William Alston ',
+  ' Paul Benacerraf',
+  '  Ross Cameron',
+  '       Gilles Deleuze',
+  '  Arda Denkel ',
+  '  Michael Devitt',
+  '  Kit Fine',
+  ' Nelson Goodman',
+  'David Kolb',
+  '   Saul Kripke',
+  '  Trenton Merricks',
+  '  Jay Rosenberg',])); //'ADGJKMNPRSTW'
+
+
